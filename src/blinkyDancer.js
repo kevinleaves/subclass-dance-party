072f3bd26deb2bbcec818ea9c0ae52f5a6eaf97e
Kevin Le <iamkevinvle@gmail.com>
2022-12-19 17:02:52 -0800

    refactor makeDancer and makeBlinkyDancer to pseudoclassical

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,43 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+var MakeBlinkyDancer = function(top, left, timeBetweenSteps) {
+  // call the parent class
+  makeDancer.call(this, top, left, timeBetweenSteps)
+
+  // var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
-  var oldStep = blinkyDancer.step;
 
-  blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
 
-  return blinkyDancer;
+  // return blinkyDancer;
 };
+// set the prototype
+MakeBlinkyDancer.prototype = Object.create(makeDancer.prototype)
+// set the constructor
+MakeBlinkyDancer.prototype.constructor = MakeBlinkyDancer;
+
+MakeBlinkyDancer.prototype.step = function() {
+  // call the old version of step at the beginning of any call to this new version of step
+  makeDancer.prototype.step.call(this)
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.toggle();
+};
+
+
+
+/*
+var MakeBlinkyDancer = function(top, left, timeBetweenSteps) {
+  makeDancer.call(this, top, left, timeBetweenSteps)
+  this.top = top
+  this.left = left
+  this.timeBetweenSteps = timeBetweenSteps
+}
+
+MakeBlinkyDancer.prototype.makeDancer = function(top, left, timeBetweenSteps)
+
+
+*/
+//
+// add methods to prototype
